ba47c65a1e88144a7992a4088e41e49dcfb25bac
angemare <a.stragies.com>
2026-06-04 19:50:41 +0200

    add übung 2 - pytest

--- Testautomatisierung/pytest/test_count_word_matches.py
+++ Testautomatisierung/pytest/test_count_word_matches.py
@@ -6,6 +6,7 @@ from count_word_matches import count_word_matches
 Basis-Tests mit Parametrisierung
 Schreibe einen parametrisierten Test, um die Funktion für einfache, 
 gemischte und einfache Randfallszenarien zu validieren.
+
 """
 @pytest.mark.parametrize("text, target, expected", [
     ("The cat sat on the mat", "cat", 1),
@@ -21,4 +22,28 @@ def test_count_word_matches(text, target, expected):
     assert count_word_matches(text, target) == expected
 
 
-print("hello patrick")
+"""### Übung 2: Testen von Randfällen (Edge Case Testing)
+
+Erstelle ein Fixture, das häufige Randfall-Eingaben bereitstellt, und teste die Funktion mit parametrisierten Tests.
+
+Fokussiere auf leere Eingaben, Leerzeichen und Interpunktion.
+
+"""
+
+
+#@pytest.fixture
+#def count_word_matches():
+  # return
+
+@pytest.mark.parametrize("text, target, expected", [
+    ("", "word", 0),
+    ("hello world", "", 0),
+    ("", "", 0),
+    ("hello world", "world", 1), # zusätzliche leerzeichen ignoriert
+    (" cat ", "cat", 1),
+    ("cat,dog cat","cat", 1),
+    ("x y z", "x", 1)
+ ])
+
+def test_count_word_matches_target(text, target, expected):
+    assert count_word_matches(text, target) == expected
