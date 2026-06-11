970173bb3e722f3e6c6315d9637137a7d228ff0f
angemare <a.stragies.com>
2026-06-12 00:04:03 +0200

    add test_register.py

---
+++ Testautomatisierung/selenium/test_register.py
@@ -0,0 +1,61 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from _pytest.fixtures import fixture
+
+"""
+Schreibe ein Selenium-Skript, das Folgendes durchführt:
+
+1. Browser starten
+2. Zur URL navigieren: http://automationexercise.com
+3. Überprüfen, dass die Startseite erfolgreich sichtbar ist
+4. Auf die Schaltfläche **„Signup / Login“** klicken
+5. Überprüfen, dass **„New User Signup!“** sichtbar ist
+6. Namen und E-Mail-Adresse eingeben
+7. Auf die Schaltfläche **„Signup“** klicken
+8. Überprüfen, dass **„ENTER ACCOUNT INFORMATION“** sichtbar ist
+9. Details ausfüllen: Titel, Name, E-Mail, Passwort, Geburtsdatum
+10. Kontrollkästchen **„Sign up for our newsletter!“** auswählen
+11. Kontrollkästchen **„Receive special offers from our partners!“
+** auswählen
+12. Details ausfüllen: Vorname, Nachname, Firma, Adresse, Adresse2, 
+Land, Bundesstaat, Stadt, Postleitzahl, Handynummer
+13. Auf die Schaltfläche **„Create Account“** klicken
+14. Überprüfen, dass **„ACCOUNT CREATED!“** sichtbar ist
+15. Auf die Schaltfläche **„Continue“** klicken
+16. Überprüfen, dass **„Logged in as username“** sichtbar ist
+17. Auf die Schaltfläche **„Delete Account“** klicken
+18. Überprüfen, dass **„ACCOUNT DELETED!“** sichtbar ist und 
+auf **„Continue“** klicken"""
+
+@fixture
+def driver():
+    # instantiate a WebDriver object
+    driver = webdriver.Edge()
+    driver.implicitly_wait(5)
+    # function will continue due to yield
+    yield driver
+    # end session
+    driver.quit()
+
+# test funktion erstellen
+def test_registration_automation(driver):
+    # 1./ 2./ start browser and navigate to website automationexercise.com
+    driver.get("https://automationexercise.com/")
+
+    # 3./ check if homepage is visible
+    assert "Automation Exercise" in driver.title
+
+    # 4./ click signup and login
+    driver.find_element(By.CSS_SELECTOR, "a[href='/login']").click()
+
+    # 5./ check if New User Signup is visible
+    new_sign_up_header = driver.find_element(By.XPATH, "//*[@ID='form']/div/div/div[3]/div/h2[text()='New User Signup!']")
+    assert new_sign_up_header.is_displayed(), "New User Signup!"
+
+    #6./ 7./ fill in username & email & click on signup
+    driver.find_element(By.CSS_SELECTOR, "signup-name").send_keys("Testuser")
+    driver.find_element(By.CSS_SELECTOR, "signup-email").send_keys("testuser000@testing.com")
+    driver.find_element(By.CSS_SELECTOR, "signup-button").click()
+
+
+
--- Testautomatisierung/selenium/test_website_login_product_name.py
+++ Testautomatisierung/selenium/test_website_login_product_name.py
@@ -8,7 +8,9 @@ import pytest
 def driver():
     # instantiate a WebDriver object
     driver = webdriver.Edge()
+    driver.implicitly_wait(20)
 
+    # function will continue due to yield
     yield driver
     # end session
     driver.quit()
@@ -27,18 +29,15 @@ def driver():
 def test_website_login_and_product_name(driver, usernames):
     # navigate to saucedemo Website with get method
     driver.get("https://www.saucedemo.com/")
-    driver.implicitly_wait(3000)
 
     # find and interact with accepted usernames and password
     driver.find_element(By.ID, "user-name").send_keys(usernames)
-    driver.implicitly_wait(3000)
 
     driver.find_element(By.ID, "password").send_keys("secret_sauce")
-    driver.implicitly_wait(3000)
 
     login_button = (driver.find_element(By.ID, "login-button"))
     login_button.click()
-    driver.implicitly_wait(3000)
+
 
     # find product name by checking its class name
     product_name = driver.find_element(By.CLASS_NAME, "inventory_item_name").text
@@ -46,17 +45,15 @@ def test_website_login_and_product_name(driver, usernames):
 
     #  test login with not successful login usernames
     if usernames == "locked_out_user":
-        error_message = driver.find_element(By.CSS_SELECTOR, "error").text
+        error_message = driver.find_element(By.CSS_SELECTOR, "[data-test='error']").text
         assert "Sorry, this user has been locked out" in error_message
+
     else:
         # test login with successful login usernames
         assert "Swag Labs" in driver.title
 
-
     # test availability of product_name Sauce Labs Backpack
     assert product_name == "Sauce Labs Backpack"
-
-
 
 
 
